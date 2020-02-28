042a7acf5811d00c1c8dfd5be9ebf106a401d5c2
John-Bomann <bomannj@wwu.edu>
2020-02-27 18:04:47 -0800

    Fixed MatrixReader

--- CSCI145_Homework2/src/Homework2.java
+++ CSCI145_Homework2/src/Homework2.java
@@ -1,5 +1,9 @@
 
 public class Homework2 {
+    public static void main(String args[]) {
+        MatrixReader matrixReader = new MatrixReader();
+        matrixReader.read("C:\\Users\\bomannj\\IdeaProjects\\CSCI145Homework2\\CSCI145_Homework2\\src\\matrixA.txt");
+    }
     public void run() {
 
     }
--- CSCI145_Homework2/src/MatrixReader.java
+++ CSCI145_Homework2/src/MatrixReader.java
@@ -1,27 +1,35 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class MatrixReader {
 
     public SparseMatrix read(String file) {
-        Scanner scan = new Scanner(file);
-        String numRowStr = scan.nextLine();
-        int numRow = Integer.parseInt(numRowStr);
-        String numColStr = scan.nextLine();
-        int numCol = Integer.parseInt(numColStr);
-        // Creates scanner, reads first 2 lines
-        SparseMatrix sparseMatrix = new SparseMatrix(numRow, numCol);
-        int rowCount = 0;
-        while(scan.hasNext()) {
-            String nextRow = scan.nextLine();
-            String[] rowElements = nextRow.split(" ");
-            // Splits line into pairs of column positions and values
-            for(String val : rowElements) {
-                String[] element = val.split(",");
-                // Splits pairs into column positions and values
-                int colPosition = Integer.parseInt(element[0]);
-                int value = Integer.parseInt(element[1]);
-                sparseMatrix.insert(rowCount, colPosition, value);
+        try {
+            File matrix = new File(file);
+            Scanner scan = new Scanner(matrix);
+            String numRowStr = scan.nextLine();
+            int numRow = Integer.parseInt(numRowStr);
+            String numColStr = scan.nextLine();
+            int numCol = Integer.parseInt(numColStr);
+            // Creates scanner, reads first 2 lines
+            SparseMatrix sparseMatrix = new SparseMatrix(numRow, numCol);
+            int rowCount = 0;
+            while (scan.hasNext()) {
+                String nextRow = scan.nextLine();
+                String[] rowElements = nextRow.split(" ");
+                // Splits line into pairs of column positions and values
+                for (String val : rowElements) {
+                    String[] element = val.split(",");
+                    // Splits pairs into column positions and values
+                    int colPosition = Integer.parseInt(element[0]);
+                    int value = Integer.parseInt(element[1]);
+                    System.out.println(colPosition + " " + value);
+                    sparseMatrix.insert(rowCount, colPosition, value);
+                }
             }
+            return sparseMatrix;
+        } catch (Exception e) {
+            System.out.println(e);
         }
         return null;
     }
